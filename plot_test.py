00c5a4ffe712fcd469cf38fbbd0455c9b0246147
Anupal Mishra <anupal.pm@gmail.com>
2024-01-03 13:24:01 +0000

    switch to absolute usage

--- plot_test.py
+++ plot_test.py
@@ -19,24 +19,26 @@ df.set_index("TIME", inplace=True)
 # filter weekdays
 # df = df[df['WEEKEND'] == 1]
 
-# take total weekly total
-station_avg_df = df.resample("W").sum()
+# take total weekly total for USAGE
+station_sum_df = df.resample("W").sum()
+station_sum_df.reset_index(drop=False, inplace=True)
+
+# take average weekly for utilization
+station_avg_df = df.resample("W").mean()
 station_avg_df.reset_index(drop=False, inplace=True)
 
 
-trace_usage_out = go.Scatter(x=station_avg_df["TIME"], y=station_avg_df["USAGE_OUT"], mode='lines+markers', name='Example')
-trace_usage_in = go.Scatter(x=station_avg_df["TIME"], y=station_avg_df["USAGE_IN"], mode='lines+markers', name='Example')
-trace_utilization = go.Scatter(x=station_avg_df["TIME"], y=station_avg_df["UTILIZATION"], mode='lines+markers', name='Example')
+trace_usage = go.Scatter(x=station_sum_df["TIME"], y=station_sum_df["USAGE"], mode='lines+markers', name='Usage')
+trace_utilization = go.Scatter(x=station_avg_df["TIME"], y=station_avg_df["UTILIZATION"], mode='lines+markers', name='Utilization')
 
-fig = make_subplots(rows=3, cols=1, subplot_titles=('USAGE OUT', 'USAGE IN', 'UTILIZATION'))
+fig = make_subplots(rows=2, cols=1, subplot_titles=("USAGE", "UTILIZATION"))
 
-fig.add_trace(trace_usage_out, row=1, col=1)
-fig.add_trace(trace_usage_in, row=2, col=1)
-fig.add_trace(trace_utilization, row=3, col=1)
+fig.add_trace(trace_usage, row=1, col=1)
+fig.add_trace(trace_utilization, row=2, col=1)
 
 
 # Update layout (optional)
-fig.update_layout(title='Multiple Plots')
+fig.update_layout(title='Bike usage and station utilization')
 
 # Display the figure
 fig.show()
